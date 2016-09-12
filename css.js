b76a086a100da35084c198aed29a9c3de3d3418f
annakawaguchi5 <k14jk037@st.kyusan-u.ac.jp>
2016-09-12 19:19:42 +0900

    9/12

--- css.js
+++ css.js
@@ -3,29 +3,69 @@
  * ポップアップ
  */
 $(function() {
+	var dialog, form,
+
+	/*year = $( "#year" ),
+	stime = $( "#stime" ),
+	ltime = $( "#ltime" );*/
+	allFields = $( [] ).add( year ).add( stime ).add( ltime );
+
 	// 「新規作成」ボタンがクリックされたらダイアログを表示
-	$("#new").click(function() {
-		$("#dialog-form").dialog("open");
-		return false;
+
+
+	$( "#new" ).button().on( "click", function() {
+		$('#year').val('');
+		$('#stime').val('');
+		$('#ltime').val('');
+		dialog.dialog( "open" );
 	});
 
+	function addYear() {
+		var valid = true;
+		allFields.removeClass( "ui-state-error" );
+		if ( valid ) {
+
+			$("<td>"+$('#year').val()+"年度</td>").insertAfter('#form2 .year');
+			$("<td>"+$('#stime').val()+"</td>").insertAfter('#form2 .stime');
+			$("<td>"+$('#ltime').val()+"</td>").insertAfter('#form2 .ltime');
+			/*
+			$("<td>"+$('#year').val()+"年度</td>").replaceAll('#form2 .year');
+			$("<td>"+$('#stime').val()+"</td>").replaceAll('#form2 .stime');
+			$("<td>"+$('#ltime').val()+"</td>").replaceAll('#form2 .ltime');*/
+
+			valid = false;
+		}
+		return valid;
+		allFields.removeData();
+		//allFields.detach('year');
+	}
+
 	// 表示するフォームは 'dialog-form' という ID で定義します。
-	$("#dialog-form").dialog({
+	dialog = $("#dialog-form").dialog({
 		autoOpen: false,
 		height: 480,
 		width: 460,
 		modal: true,
 		buttons: {  // ダイアログに表示するボタンと処理
-			"新規作成": function() {
+			"新規作成": function(year, stime, ltime) {
 				$(this).dialog("close");
 				$("#dialog-form-check").dialog("open");
+				addYear();
 			},
 			"キャンセル": function() {
-
 				$(this).dialog("close");
+				Close();
 			}
 		}
+
 	});
+
+	form = dialog.find( "form2" ).on( "submit", function( event ) {
+		event.preventDefault();
+		addYear();
+	});
+
+
 
 	$("#dialog-form-check").dialog({
 		autoOpen: false,
@@ -34,17 +74,36 @@ $(function() {
 		modal: true,
 		buttons: {  // ダイアログに表示するボタンと処理
 			"新規作成": function() {
+				//新規作成を実行
+				//document.form.submit();
 				$(this).dialog("close");
+				Close();
 				return false;
+
 			},
 			"キャンセル": function() {
 				$(this).dialog("close");
+				Close();
 			}
 		}
 	});
 
-
+	function Close() {
+		form.reset();
+		allFields.val("").removeClass( "ui-state-error" );
+	}
 });
+function showValues() {
+    var fields = $(":input").serializeArray();
+    $("#results").empty();
+    jQuery.each(fields, function(i, field){
+      $("#results").append(field.value + " ");
+    });
+  }
+  $(":checkbox, :radio").click(showValues);
+  $("#text").click(showValues);
+  $("select").change(showValues);
+  showValues();
 
 //エラー表示
 function displayError(str) {
--- demo2.php
+++ demo2.php
@@ -1,100 +1,46 @@
-<imart type="head">
-  <title>新規登録画面</title>
-           ・・・
-</imart>
- <!-- Load library -->
- <script src="ui/libs/jquery-validation-1.9.0/jquery.validate.js"></script>
- <script src="ui/js/imui-form-util.js"></script>
- <imart type="imuiValidationRule" rule="foo/bar#hoge" rulesName="rules" messagesName="messages" />
- <script type="text/javascript">
-   (function($) {
-     $(document).ready(function() {
-       // Form の 2 度押し防止
-       imuiDisableOnSubmit('#form');
-
-       // 参照画面へ引き渡すキーの配列生成
-       var optionalData = ['user_cd'];
-
-       // 登録ボタンクリック
-       $('#register-button').click(function() {
-
-         // バリデーションチェック
-         if (imuiValidate('#form', rules, messages)) {
-           // 確認ダイアログ表示
-           imuiConfirm(
-             '<imart type="string" value=$data.dialogMessages.message escapeJs="true" />', // メッセージ
-             '<imart type="string" value=$data.dialogMessages.title escapeJs="true" />',  // タイトル
-             function() { // OKクリック時のコールバック関数
-               // Ajax でのデータ送信と次画面への遷移
-               imuiAjaxSubmit('#form', 'POST', 'json', 'reference/list/views/list');
-             }
-           );
-         }
-       });
-     });
-   })(jQuery);
- </script>
-<!-- 画面タイトル -->
-<div class="imui-title">
-  <h1>新規登録画面</h1>
-</div>
-
-<!-- ツールバー -->
-<div class="imui-toolbar-wrap">
-  <div class="imui-toolbar-inner">
-    <ul class="imui-list-toolbar">
-      <!-- 戻るボタン -->
-      <li><a href="#" class="imui-toolbar-icon" title="戻る"><span class="im-ui-icon-common-16-back"></span></a></li>
-    </ul>
-  </div>
-</div>
-
-<!-- コンテンツエリア -->
-<div class="imui-form-container-narrow">
-  <!-- h2 -->
-  <div class="imui-chapter-title">
-    <h2>入力フォームのタイトル（必要に応じて配置）</h2>
-  </div>
-
-  <form id="form" method="POST" class="target_form mt-10" action="hoge/register" onsubmit="return false;">
-    <table class="imui-form">
-      <tr>
-        <th><label>項目名1</label></th>
-        <td><imart type="imuiTextbox" id="textbox" name="textbox" style="width: 200px;" /></td>
-      </tr>
-      <tr>
-        <th><label>項目名2</label></th>
-        <td><imart type="imuiPassword" id="password" name="password" style="width: 200px;" /></td>
-      </tr>
-      <tr>
-        <th><label>項目名3</label></th>
-        <td><imart type="imuiTextArea" id="textarea" name="textarea" width="350px" height="50px" /></td>
-      </tr>
-      <tr>
-        <th><label>項目名4</label></th>
-        <td>
-          <imart type="imuiCheckbox" id="checkbox_1" name="checkbox" label="チェック1"  />
-          <imart type="imuiCheckbox" id="checkbox_2" name="checkbox" label="チェック2"  />
-          <imart type="imuiCheckbox" id="checkbox_3" name="checkbox" label="チェック3"  />
-        </td>
-      </tr>
-      <tr>
-        <th><label>項目名5</label></th>
-        <td>
-          <imart type="imuiRadio" id="radio_1" name="radio" label="ラジオ1" />
-          <imart type="imuiRadio" id="radio_2" name="radio" label="ラジオ2" />
-          <imart type="imuiRadio" id="radio_3" name="radio" label="ラジオ3" />
-        </td>
-      </tr>
-      <tr>
-        <th><label>項目名6</label></th>
-        <td>
-          <imart type="imuiSelect" id="inputItem6" name="inputItem6" width="200px" />
-        </td>
-      </tr>
-    </table>
-    <div class="imui-operation-parts">
-      <imart type="imuiButton" value="登録" id="register-button" class="imui-large-button" />
-    </div>
-  </form>
-</div>
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style>
+      body, select { font-size:12px; }
+      form { margin:5px; }
+      p { color:red; margin:5px; }
+      b { color:blue; }
+      </style>
+      <script src="http://code.jquery.com/jquery-latest.js"></script>
+    </head>
+    <body>
+      <p><b>Results:</b> <span id="results"></span></p>
+      <form>
+        <select name="single">
+          <option>Single</option>
+          <option>Single2</option>
+        </select>
+        <select name="multiple" multiple="multiple">
+          <option selected="selected">Multiple</option>
+          <option>Multiple2</option>
+          <option selected="selected">Multiple3</option>
+        </select><br/>
+        <input type="checkbox" name="check" value="check1" id="ch1"/>
+        <label for="ch1">check1</label>
+        <input type="checkbox" name="check" value="check2" checked="checked" id="ch2"/>
+        <label for="ch2">check2</label>
+        <input type="radio" name="radio" value="radio1" checked="checked" id="r1"/>
+        <label for="r1">radio1</label>
+        <input type="radio" name="radio" value="radio2" id="r2"/>
+        <label for="r2">radio2</label>
+      </form>
+    <script>
+      function showValues() {
+        var fields = $(":input").serializeArray();
+        $("#results").empty();
+        jQuery.each(fields, function(i, field){
+          $("#results").append(field.value + " ");
+        });
+      }
+      $(":checkbox, :radio").click(showValues);
+      $("select").change(showValues);
+      showValues();
+    </script>
+    </body>
+    </html>
