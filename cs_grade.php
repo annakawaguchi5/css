7a444c554fcd593dfd6a6e5f796e139abafd5f29
annakawaguchi5 <k14jk037@st.kyusan-u.ac.jp>
2016-09-14 22:16:28 +0900

    9/14

--- cs_grade.php
+++ cs_grade.php
@@ -4,7 +4,7 @@ if ( isset($_SESSION['urole']) and $_SESSION['urole']==1 ) {
 	$uid   = $_SESSION['uid'];   // 認証済みのユーザID
 	$uname = $_SESSION['uname']; // 認証済みのユーザ名
 	echo "<h2>成績確認</h2>";
-	$sql="SELECT uname, halfgp, halfgpa, gp, gpa
+	$sql="SELECT uname, halfgp, halfgpa, allgp, allgpa
 FROM tb_gp
 NATURAL JOIN tb_user
 WHERE uid = '$uid'";
@@ -13,29 +13,13 @@ WHERE uid = '$uid'";
 $rs = mysql_query($sql,$conn);
 if(!$rs)die('エラー： '.mysql_error());
 $row = mysql_fetch_array($rs);
-echo '氏名：'.$row['uname'];
-echo '<table border=2>';
-echo '<tr><th></th><th>修得単位数</th><th>ＧＰＡ</th></tr>';
-while($row){
-	//前期
-	echo '<td>前期</td>';
-	echo '<td>'.$row['halfgp'].'</td>';
-	echo '<td>'.$row['halfgpa'].'</td>';
-	echo'</tr>';
-	//後期
-	echo '<td>後期</td>';
-	echo '<td>'.$row['gp'].'</td>';
-	echo '<td>'.$row['gpa'].'</td>';
-	echo'</tr>';
-	//合計
-	$allgp = $row['halfgp']+$row['gp'];
-	$allgpa = $row['halfgpa']+$row['gpa'];
-	echo '<td>合計</td>';
-	echo '<td>'.$allgp.'</td>';
-	echo '<td>'.$allgpa.'</td>';
-	echo'</tr>';
-	$row=mysql_fetch_array($rs);
+
+echo '<table border=0 class="table table-hover">';
+echo '<tr class="info"><th></th><th>取得単位数</th><th>GPA</th></tr>';
+while ($row) {
+	echo '<tr><td>前期</td><td>'.$row['halfgp'].'</td><td>'.$row['halfgpa'].'</td></tr>';
+	echo '<tr><td>年間</td><td>'.$row['allgp'].'</td><td>'.$row['allgpa'].'</td></tr>';
+	$row = mysql_fetch_array($rs) ;
 }
 echo '</table>';
-include('page_footer.php');
 ?>
