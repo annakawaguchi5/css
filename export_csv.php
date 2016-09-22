55a995e0546e7a535d3e2058b7bd9b2c989f1660
shouichiK <k14jk032@st.kyusan-u.ac.jp>
2016-09-22 21:59:54 +0900

    no message

---
+++ export_csv.php
@@ -0,0 +1,76 @@
+<?php
+include('page_header.php');
+include_once('db_inc.php');
+
+
+//submitボタンが押され、hiddenで持たせた値がPOSTされたら処理が始まります。
+if(isset($_POST['csv01'])){
+
+
+
+ // ファイル名
+    $file_path = 'export.csv';
+
+    // CSVに出力するタイトル行
+    $export_csv_title = array( "学籍番号", "氏名", "選択コース", );
+
+    // CSVに出力する内容
+    $sql ="SELECT uid, uname, cname
+FROM tb_user
+NATURAL JOIN tb_decide
+NATURAL JOIN tb_course
+WHERE urole =  '1'
+AND tb_user.uid = tb_decide.uid ";
+    $res = mysql_query( $sql, $conn );
+
+    if( touch($file_path) ){
+
+        // オブジェクト生成
+        $file = new SplFileObject( $file_path );
+
+        // タイトル行のエンコードをSJIS-winに変換（一部環境依存文字に対応用）
+        foreach( $export_csv_title as $key => $val ){
+
+            $export_header[$key] = mb_convert_encoding($val, 'SJIS-win', 'UTF-8');
+
+        }
+
+        // エンコードしたタイトル行を配列ごとCSVデータ化
+        $file->fputcsv($export_header);
+
+        while( $row = mysql_fetch_assoc( $res ) ){
+
+            $export_arr = "";
+
+            // 内容行のエンコードをSJIS-winに変換（一部環境依存文字に対応用）
+            foreach( $row as $key => $val ){
+
+                $export_arr[$key] = mb_convert_encoding($val, 'SJIS-win', 'UTF-8');
+            }
+
+            // エンコードした内容行を配列ごとCSVデータ化
+            $file->fputcsv($export_arr);
+
+        }
+
+    }
+
+    // ダウンロード用
+    header("Pragma: public"); // required
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: private",false); // required for certain browsers
+    header("Content-Type: application/force-download");
+    header("Content-Disposition: attachment; filename=\"".basename($file_path)."\";" );
+    header("Content-Transfer-Encoding: binary");
+    readfile("$file_path");
+
+}
+
+
+//HTMLファイル読み込み
+include "export_csv.html";
+
+
+include('page_footer.php');
+?>
