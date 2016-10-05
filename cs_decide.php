dcb62b42746096b8d7781f02abd03dd7fa6f184f
shouichiK <k14jk032@st.kyusan-u.ac.jp>
2016-10-06 03:34:53 +0900

    no message

--- cs_decide.php
+++ cs_decide.php
@@ -144,264 +144,271 @@ $row = mysql_fetch_array($rs) ;
 
 
 
-<form action="cs_decide_do.php" name="frmPeropero_sougo" class="form-horizontal" method="POST">
-<?php
-echo '<table border=0 class="table table-striped table-hover table-bordered">';
-echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース決定</th></tr>';//cssで決定ボタンを追加
+<form action="cs_decide_do.php" name="frmPeropero_sougo"
+	class="form-horizontal" method="POST">
+	<?php
+	echo '<table border=0 class="table table-striped table-hover table-bordered">';
+	echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース結果</th></tr>';//cssで決定ボタンを追加
 
-//決定済み確認用////////////////////
-/*
- $sql_decide = "SELECT uid
- FROM tb_decide
- NATURAL JOIN tb_user
- WHERE YEAR ='$year'
- ";
- $rs_decide = mysql_query($sql_decide, $conn);
- if (!$rs_decide) die ('エラー: ' . mysql_error());
- $row_decide = mysql_fetch_array($rs_decide) ;
- */
-//////////////////////////////////////
+	//決定済み確認用////////////////////
+	/*
+	 $sql_decide = "SELECT uid
+	 FROM tb_decide
+	 NATURAL JOIN tb_user
+	 WHERE YEAR ='$year'
+	 ";
+	 $rs_decide = mysql_query($sql_decide, $conn);
+	 if (!$rs_decide) die ('エラー: ' . mysql_error());
+	 $row_decide = mysql_fetch_array($rs_decide) ;
+	 */
+	//////////////////////////////////////
 
-while($row){
-
-
-
-
-	//学生情報を検索
-
-	$cid=$row['cid'];
-	$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
-	if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
-		$judge = "◯";
-	}else{
-		$judge = "✕";
-	}
-	//希望提出済みコース未決定の学生情報を検索
-	$class1 = "default";
-	$class2 = "default";
+	while($row){
 
 
-	//決定されてるかどうか判別
+
+
+		//学生情報を検索
+
+		$cid=$row['cid'];
+		$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
+		if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
+			$judge = "◯";
+		}else{
+			$judge = "✕";
+		}
+		//希望提出済みコース未決定の学生情報を検索
+		$class1 = "default";
+		$class2 = "default";
+
+
+		//決定されてるかどうか判別
 	$sql_decide = "SELECT uid,cid
 FROM tb_decide
 NATURAL JOIN tb_user
 WHERE YEAR ='$year'
 ";
-	$rs_decide = mysql_query($sql_decide, $conn);
-	if (!$rs_decide) die ('エラー: ' . mysql_error());
-	$row_decide = mysql_fetch_array($rs_decide) ;
+		$rs_decide = mysql_query($sql_decide, $conn);
+		if (!$rs_decide) die ('エラー: ' . mysql_error());
+		$row_decide = mysql_fetch_array($rs_decide) ;
 
 
 
-	while($row_decide){
-		$user=$row_decide['uid'];
-		$user_decide=$row_decide['cid'];
-		if($row['uid']==$user){
-			$act = 'update';    // すでに登録したため「再登録」とする
-			if($user_decide == 1){
-				$class1 = "danger";
-				$class2 = "default";
-			}else if($user_decide == 2){
-				$class1 = "default";
-				$class2 = "primary";
+		while($row_decide){
+			$user=$row_decide['uid'];
+			$user_decide=$row_decide['cid'];
+			if($row['uid']==$user){
+				$act = 'update';    // すでに登録したため「再登録」とする
+				if($user_decide == 1){
+					$class1 = "danger";
+					$class2 = "default";
+				}else if($user_decide == 2){
+					$class1 = "default";
+					$class2 = "primary";
+				}
+
 			}
 
+			$row_decide = mysql_fetch_array($rs_decide) ;
 		}
 
-		$row_decide = mysql_fetch_array($rs_decide) ;
-	}
-	$act;
-	$uid=$row['uid'];
-	$uname=$row['uname'];
-	?>
+		$act;
+		$uid=$row['uid'];
+		$uname=$row['uname'];
+		echo '<tr>'?>
 
 
-	<input type="hidden" name="act" value="<?php echo $act;?>">
-	<td><input type="checkbox" name="chk_sougo[]" value="<?php echo $uid; ?>"></td>
+	<input type="hidden" name="act[]" value="<?php echo $act;?>">
+	<td><input type="checkbox" name="chk_sougo[]"
+		value="<?php echo $uid; ?>"></td>
 
 
 
 
-	<?php
+		<?php
 
 
-	echo '<td>' . $row['uid'] . '</td>';
-	echo '<td>' . $row['uname']. '</td>';
-	echo '<td>' . $row['cname'] . '</td>';
-	echo '<td>' . $row['note'] . '</td>';
-	echo '<td>' . $row['allgp'] . '</td>';
-	echo '<td>' . $row['allgpa'] . '</td>';
-	echo '<td style="color:red">' . $judge . '</td>';
-	if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
-		//教員（権限なし）としてログインしているなら
-		$disabled = "disabled";
-	}else{
-		$disabled = "";
-	}
-	echo'<td>
-	<form action="cs_decide_do.php?uid='.$row['uid'].'" class="form-horizontal" method="post">
-		<input type="hidden" name="uid" value="'.$row['uid'].'">
-		<input type="hidden" name="act" value="'.  $act .'">
-		<input type="hidden" name="uname" value="'.$row['uname'].'">
+		echo '<td>' . $row['uid'] . '</td>';
+		echo '<td>' . $row['uname']. '</td>';
+		echo '<td>' . $row['cname'] . '</td>';
+		echo '<td>' . $row['note'] . '</td>';
+		echo '<td>' . $row['allgp'] . '</td>';
+		echo '<td>' . $row['allgpa'] . '</td>';
+		echo '<td style="color:red">' . $judge . '</td>';
+		if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
+			//教員（権限なし）としてログインしているなら
+			$disabled = "disabled";
+		}else{
+			$disabled = "disabled";
+		}
+		echo'<td>
+
+
+
+
 		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" '.$disabled.'>応用</button>
 		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" '.$disabled.'>総合</button>
-		</form></td>';
-	echo '</tr>';
+		</td>';
+		echo '</tr>';
 
 
-	$row = mysql_fetch_array($rs) ;
-}echo '</table>';
+		$row = mysql_fetch_array($rs) ;
+	}echo '</table>';
 
-?>
-		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" <?php $disabled ?> >応用</button>
-		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" <?php $disabled ?> >総合</button>
-		<a href="javascript:void(0)"
+	?>
+	<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1"
+	<?php $disabled ?>>応用</button>
+	<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2"
+	<?php $disabled ?>>総合</button>
+	<a href="javascript:void(0)"
 		onClick="checkbox_changer_sougo(true); return false;">全てチェック</a>| <a
 		href="javascript:void(0)"
 		onClick="checkbox_changer_sougo(false); return false;">全てのチェックを外す</a><br />
 
-	</form>
+</form>
 
-<?php
-///////////////////////////////////////////////////////////////////////////
+	<?php
+	///////////////////////////////////////////////////////////////////////////
 
-//応用コース
+	//応用コース
 
-//コース名のみ
+	//コース名のみ
 
-$sql = "SELECT cname
+	$sql = "SELECT cname
 FROM tb_course
 WHERE YEAR ='$year'
 AND cid=1";
-$rs = mysql_query($sql, $conn);
-if (!$rs) die ('エラー: ' . mysql_error());
-$row = mysql_fetch_array($rs) ;
-echo "<h2>".$row['cname']."</h2>";
-///////////////////////////////////////
+	$rs = mysql_query($sql, $conn);
+	if (!$rs) die ('エラー: ' . mysql_error());
+	$row = mysql_fetch_array($rs) ;
+	echo "<h2>".$row['cname']."</h2>";
+	///////////////////////////////////////
 
-//学生の値
-$sql = "SELECT uid, cid,uname, cname, note, allgp, allgpa
+	//学生の値
+	$sql = "SELECT uid, cid,uname, cname, note, allgp, allgpa
 FROM tb_user
 NATURAL JOIN tb_gp
 NATURAL JOIN tb_entry
 NATURAL JOIN tb_course
 WHERE YEAR ='$year'
 AND cid=1";
-$rs = mysql_query($sql, $conn);
-if (!$rs) die ('エラー: ' . mysql_error());
-$row = mysql_fetch_array($rs) ;
+	$rs = mysql_query($sql, $conn);
+	if (!$rs) die ('エラー: ' . mysql_error());
+	$row = mysql_fetch_array($rs) ;
 
 
 
-?>
+	?>
 
-<form action="cs_decide_do.php" name="frmPeropero_ouyo" class="form-horizontal" method="POST">
-
-
-
-
-<?php
-echo '<table border=0 class="table table-striped table-hover table-bordered">';
-echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース決定</th></tr>';//cssで決定ボタンを追加
+<form action="cs_decide_do.php" name="frmPeropero_ouyo"
+	class="form-horizontal" method="POST">
 
 
 
-//////////////////////////////////////
 
-while($row){
-
-	//学生情報を検索
-
-	$cid=$row['cid'];
-
-	$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
-	if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
-		$judge = "◯";
-	}else{
-		$judge = "✕";
-	}
-
-	//希望提出済みコース未決定の学生情報を検索
+	<?php
+	echo '<table border=0 class="table table-striped table-hover table-bordered">';
+	echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース決定</th></tr>';//cssで決定ボタンを追加
 
 
 
-	$class1 = "default";
-	$class2 = "default";
+	//////////////////////////////////////
+
+	while($row){
+
+		//学生情報を検索
+
+		$cid=$row['cid'];
+
+		$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
+		if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
+			$judge = "◯";
+		}else{
+			$judge = "✕";
+		}
+
+		//希望提出済みコース未決定の学生情報を検索
 
 
-	$sql_decide = "SELECT uid,cid
+
+		$class1 = "default";
+		$class2 = "default";
+
+
+		$sql_decide = "SELECT uid,cid
 FROM tb_decide
 NATURAL JOIN tb_user
 WHERE YEAR ='$year'
 ";
-	$rs_decide = mysql_query($sql_decide, $conn);
-	if (!$rs_decide) die ('エラー: ' . mysql_error());
-	$row_decide = mysql_fetch_array($rs_decide) ;
+		$rs_decide = mysql_query($sql_decide, $conn);
+		if (!$rs_decide) die ('エラー: ' . mysql_error());
+		$row_decide = mysql_fetch_array($rs_decide) ;
 
 
 
-	while($row_decide){
-		$user=$row_decide['uid'];
-		$user_decide=$row_decide['cid'];
-		if($row['uid']==$user){
-			$act = 'update';    // すでに登録したため「再登録」とする
-			if($user_decide == 1){
-				$class1 = "danger";
-				$class2 = "default";
-			}else if($user_decide == 2){
-				$class1 = "default";
-				$class2 = "primary";
+		while($row_decide){
+			$user=$row_decide['uid'];
+			$user_decide=$row_decide['cid'];
+			if($row['uid']==$user){
+				$act = 'update';    // すでに登録したため「再登録」とする
+				if($user_decide == 1){
+					$class1 = "danger";
+					$class2 = "default";
+				}else if($user_decide == 2){
+					$class1 = "default";
+					$class2 = "primary";
+				}
+
 			}
 
+			$row_decide = mysql_fetch_array($rs_decide) ;
 		}
 
-		$row_decide = mysql_fetch_array($rs_decide) ;
-	}
 
-
-	$act;
-	$uid=$row['uid'];
-	$uname=$row['uname'];
-	echo '<tr>'?>
-	<input type="hidden" name="act" value="<?php echo $act;?>">
-	<td><input type="checkbox" name="chk_ouyo[]" value="<?php echo $uid; ?>"></td>
-	</form>
-	<?php
-	echo '<td>' . $row['uid'] . '</td>';
-	echo '<td>' . $row['uname']. '</td>';
-	echo '<td>' . $row['cname'] . '</td>';
-	echo '<td>' . $row['note'] . '</td>';
-	echo '<td>' . $row['allgp'] . '</td>';
-	echo '<td>' . $row['allgpa'] . '</td>';
-	echo '<td style="color:red">' . $judge . '</td>';
-	if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
-		//教員（権限なし）としてログインしているなら
-		$disabled = "disabled";
-	}else{
-		$disabled = "";
-	}
-	echo'<td>
-	<form action="cs_decide_do.php?uid='.$row['uid'].'" class="form-horizontal" method="post">
-		<input type="hidden" name="uid" value="'.$row['uid'].'">
-		<input type="hidden" name="act" value="'.  $act .'">
-		<input type="hidden" name="uname" value="'.$row['uname'].'">
+		$act;
+		$uid=$row['uid'];
+		$uname=$row['uname'];
+		echo '<tr>'?>
+	<input type="hidden" name="act[]" value="<?php echo $act;?>">
+	<td><input type="checkbox" name="chk_ouyo[]"
+		value="<?php echo $uid; ?>"></td>
+</form>
+		<?php
+		echo '<td>' . $row['uid'] . '</td>';
+		echo '<td>' . $row['uname']. '</td>';
+		echo '<td>' . $row['cname'] . '</td>';
+		echo '<td>' . $row['note'] . '</td>';
+		echo '<td>' . $row['allgp'] . '</td>';
+		echo '<td>' . $row['allgpa'] . '</td>';
+		echo '<td style="color:red">' . $judge . '</td>';
+		if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
+			//教員（権限なし）としてログインしているなら
+			$disabled = "disabled";
+		}else{
+			$disabled = "disabled";
+		}
+		echo'<td>
 		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" '.$disabled.'>応用</button>
 		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" '.$disabled.'>総合</button>
-		</form></td>';
-	echo '</tr>';
+		</td>';
+		echo '</tr>';
 
 
-	$row = mysql_fetch_array($rs) ;
-}echo '</table>';
-?>
-		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" <?php $disabled ?> >応用</button>
-		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" <?php $disabled ?> >総合</button>
-		<a href="javascript:void(0)"
-		onClick="checkbox_changer_ouyo(true); return false;">全てチェック</a>| <a
-		href="javascript:void(0)"
-		onClick="checkbox_changer_ouyo(false); return false;">全てのチェックを外す</a><br />
+		$row = mysql_fetch_array($rs) ;
+	}echo '</table>';
+	?>
+<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1"
+<?php $disabled ?>>応用</button>
+<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2"
+<?php $disabled ?>>総合</button>
+<a href="javascript:void(0)"
+	onClick="checkbox_changer_ouyo(true); return false;">全てチェック</a>
+|
+<a href="javascript:void(0)"
+	onClick="checkbox_changer_ouyo(false); return false;">全てのチェックを外す</a>
+<br />
 
-	</form>
+</form>
 <?php
 
 
@@ -423,115 +430,117 @@ $row = mysql_fetch_array($rs) ;
 
 ?>
 
-<form action="cs_decide_do.php" name="frmPeropero_mitei" class="form-horizontal" method="POST">
+<form action="cs_decide_do.php" name="frmPeropero_mitei"
+	class="form-horizontal" method="POST">
 
 
 
-<?php
-echo '<table border=0 class="table table-striped table-hover table-bordered">';
-echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース決定</th></tr>';//cssで決定ボタンを追加
-
-
-//決定済み確認用////////////////////
-
-//////////////////////////////////////
-
-
-
-while ($row) {
-
-	$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
-	if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
-		$judge = "◯";
-	}else{
-		$judge = "✕";
-	}
-	//希望提出済みコース未決定の学生情報を検索
-
-	$class1 = "default";
-	$class2 = "default";
-
-
-
-
-
+	<?php
+	echo '<table border=0 class="table table-striped table-hover table-bordered">';
+	echo '<tr class="info"><th></th><th>ユーザID</th><th>氏名</th><th>希望コース</th><th>興味のある研究分野や自己アピール</th><th>修得単位数</th><th>GPA</th><th>総合要件</th><th>コース決定</th></tr>';//cssで決定ボタンを追加
 
 
 	//決定済み確認用////////////////////
-	$sql_decide = "SELECT uid,cid
+
+	//////////////////////////////////////
+
+
+
+	while ($row) {
+
+		$act = 'insert';  //初回登録?（insert: 初回登録; update: 再登録）;
+		if($row['allgp']>=$gp && $row['allgpa']>=$gpa){
+			$judge = "◯";
+		}else{
+			$judge = "✕";
+		}
+		//希望提出済みコース未決定の学生情報を検索
+
+		$class1 = "default";
+		$class2 = "default";
+
+
+
+
+
+
+
+		//決定済み確認用////////////////////
+		$sql_decide = "SELECT uid,cid
 FROM tb_decide
 NATURAL JOIN tb_user
 WHERE YEAR ='$year'
 ";
-	$rs_decide = mysql_query($sql_decide, $conn);
-	if (!$rs_decide) die ('エラー: ' . mysql_error());
-	$row_decide = mysql_fetch_array($rs_decide) ;
+		$rs_decide = mysql_query($sql_decide, $conn);
+		if (!$rs_decide) die ('エラー: ' . mysql_error());
+		$row_decide = mysql_fetch_array($rs_decide) ;
 
 
 
-	while($row_decide){
-		$user=$row_decide['uid'];
-		$user_decide=$row_decide['cid'];
-		if($row['uid']==$user){
-			$act = 'update';    // すでに登録したため「再登録」とする
-			if($user_decide == 1){
-				$class1 = "danger";
-				$class2 = "default";
-			}else if($user_decide == 2){
-				$class1 = "default";
-				$class2 = "primary";
+		while($row_decide){
+			$user=$row_decide['uid'];
+			$user_decide=$row_decide['cid'];
+			if($row['uid']==$user){
+				$act = 'update';    // すでに登録したため「再登録」とする
+				if($user_decide == 1){
+					$class1 = "danger";
+					$class2 = "default";
+				}else if($user_decide == 2){
+					$class1 = "default";
+					$class2 = "primary";
+				}
+
 			}
 
+			$row_decide = mysql_fetch_array($rs_decide) ;
 		}
 
-		$row_decide = mysql_fetch_array($rs_decide) ;
-	}
 
 
+		$act;
+		$uid=$row['uid'];
+		$uname=$row['uname'];
+		echo '<tr>'?>
 
-
-	$uid=$row['uid'];
-	$uname=$row['uname'];
-	echo '<tr>'?>
-
-	<input type="hidden" name="act" value="<?php echo $act;?>">
-	<td><input type="checkbox" name="chk_mitei[]" value="<?php echo $uid; ?>"></td>
-	</form>
-	<?php
-	echo '<td>' . $row['uid'] . '</td>';
-	echo '<td>' . $row['uname']. '</td>';
-	echo '<td>' . "" . '</td>';
-	echo '<td>' . "" . '</td>';
-	echo '<td>' . $row['allgp'] . '</td>';
-	echo '<td>' . $row['allgpa'] . '</td>';
-	echo '<td style="color:red">' . $judge . '</td>';
-	if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
-		//教員（権限なし）としてログインしているなら
-		$disabled = "disabled";
-	}else{
-		$disabled = "";
-	}
-	echo'<td>
-	<form action="cs_decide_do.php?uid='.$row['uid'].'" class="form-horizontal" method="post">
-		<input type="hidden" name="uid" value="'.$row['uid'].'">
-		<input type="hidden" name="act" value="'.  $act .'">
-		<input type="hidden" name="uname" value="'.$row['uname'].'">
+	<input type="hidden" name="act[]" value="<?php echo $act;?>">
+	<td><input type="checkbox" name="chk_mitei[]"
+		value="<?php echo $uid; ?>"></td>
+</form>
+		<?php
+		echo '<td>' . $row['uid'] . '</td>';
+		echo '<td>' . $row['uname']. '</td>';
+		echo '<td>' . "" . '</td>';
+		echo '<td>' . "" . '</td>';
+		echo '<td>' . $row['allgp'] . '</td>';
+		echo '<td>' . $row['allgpa'] . '</td>';
+		echo '<td style="color:red">' . $judge . '</td>';
+		if ( isset($_SESSION['urole']) and $_SESSION['urole']==2 ) {
+			//教員（権限なし）としてログインしているなら
+			$disabled = "disabled";
+		}else{
+			$disabled = "disabled";
+		}
+		echo'<td>
 		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" '.$disabled.'>応用</button>
 		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" '.$disabled.'>総合</button>
-		</form></td>';
-	echo '</tr>';
-	$row = mysql_fetch_array($rs) ;
-}
-echo '</table>';
-?>
-		<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1" <?php $disabled ?> >応用</button>
-		<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2" <?php $disabled ?> >総合</button>
-		<a href="javascript:void(0)"
-		onClick="checkbox_changer_mitei(true); return false;">全てチェック</a>| <a
-		href="javascript:void(0)"
-		onClick="checkbox_changer_mitei(false); return false;">全てのチェックを外す</a><br />
+		</td>';
+		echo '</tr>';
+		$row = mysql_fetch_array($rs) ;
+	}
+	echo '</table>';
+	?>
+<button class="btn btn-'.$class1.'" type="submit" name="cid" value="1"
+<?php $disabled ?>>応用</button>
+<button class="btn btn-'.$class2.'" type="submit" name="cid" value="2"
+<?php $disabled ?>>総合</button>
+<a href="javascript:void(0)"
+	onClick="checkbox_changer_mitei(true); return false;">全てチェック</a>
+|
+<a href="javascript:void(0)"
+	onClick="checkbox_changer_mitei(false); return false;">全てのチェックを外す</a>
+<br />
 
-	</form>
+</form>
 <?php
 
 
--- cs_decide_do.php
+++ cs_decide_do.php
@@ -9,21 +9,32 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 
 	//総合コース用/////////////////////////////////////
 	if(isset($_POST['chk_sougo'])){
-		echo "sougou";
 		$chk_sougo=$_POST['chk_sougo'];
-		foreach( $chk_sougo as $c ) {
-			echo $uid= $c;
+		$act_decide=$_POST['act'];
 
-			echo $act  = $_POST['act'] ;
-			echo $cid  = $_POST['cid'] ;
+		foreach( $chk_sougo as $c=>$a  ) {
+			$output[] = array($chk_sougo[$c], $act_decide[$c]);
+		}
+		foreach($output as $k=>$f){
+			echo $uid=$f[0];
+			echo $act=$f[1];
 
-			echo $sql = "SELECT uname FROM tb_user WHERE uid='$uid'";
+
+
+
+
+
+			//echo $act  = $_POST['act'] ;
+			$cid  = $_POST['cid'] ;
+
+			$sql = "SELECT uname FROM tb_user WHERE uid='$uid'";
 			$rs = mysql_query($sql, $conn);
 			if (!$rs) die ('エラー: ' . mysql_error());
 			$row = mysql_fetch_array($rs) ;
 
 			while($row){
 				$uname=$row['uname'];
+
 				if ($act == 'insert'){//新規登録の場合
 					$sql = "INSERT INTO tb_decide VALUES('$uid','$uname','$cid')";
 					echo "<h2>総合コース：登録しました</h2>";
@@ -48,19 +59,30 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 
 
 		}
+
 		echo '<p><a href="cs_decide.php">戻る</a>';
 
 	}else
 
 	//応用コース用/////////////////////////////////////////
 	if(isset($_POST['chk_ouyo'])){
-		echo "ouyou";
 		$chk_sougo=$_POST['chk_ouyo'];
-		foreach( $chk_sougo as $c ) {
-			echo $uid= $c;
+		$act_decide=$_POST['act'];
 
-			echo $act  = $_POST['act'] ;
-			echo $cid  = $_POST['cid'] ;
+		foreach( $chk_sougo as $c=>$a  ) {
+			$output[] = array($chk_sougo[$c], $act_decide[$c]);
+		}
+		foreach($output as $k=>$f){
+			echo $uid=$f[0];
+			echo $act=$f[1];
+
+
+
+
+
+
+			//echo $act  = $_POST['act'] ;
+			$cid  = $_POST['cid'] ;
 
 			$sql = "SELECT uname FROM tb_user WHERE uid='$uid'";
 			$rs = mysql_query($sql, $conn);
@@ -68,9 +90,7 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 			$row = mysql_fetch_array($rs) ;
 
 			while($row){
-
-				echo $uname=$row['uname'];
-
+				$uname=$row['uname'];
 
 				if ($act == 'insert'){//新規登録の場合
 					$sql = "INSERT INTO tb_decide VALUES('$uid','$uname','$cid')";
@@ -96,20 +116,32 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 
 
 		}
+
 		echo '<p><a href="cs_decide.php">戻る</a>';
+
 
 	}else
 	#########################################
 
 	//未提出者用///////////////////////////////////////////////
 	if(isset($_POST['chk_mitei'])){
-		echo "miteishutu";
 		$chk_sougo=$_POST['chk_mitei'];
-		foreach( $chk_sougo as $c ) {
-			echo $uid= $c;
+		$act_decide=$_POST['act'];
 
-			echo $act  = $_POST['act'] ;
-			echo $cid  = $_POST['cid'] ;
+		foreach( $chk_sougo as $c=>$a  ) {
+			$output[] = array($chk_sougo[$c], $act_decide[$c]);
+		}
+		foreach($output as $k=>$f){
+			echo $uid=$f[0];
+			echo $act=$f[1];
+
+
+
+
+
+
+			//echo $act  = $_POST['act'] ;
+			$cid  = $_POST['cid'] ;
 
 			$sql = "SELECT uname FROM tb_user WHERE uid='$uid'";
 			$rs = mysql_query($sql, $conn);
@@ -117,9 +149,7 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 			$row = mysql_fetch_array($rs) ;
 
 			while($row){
-
-				echo $uname=$row['uname'];
-
+				$uname=$row['uname'];
 
 				if ($act == 'insert'){//新規登録の場合
 					$sql = "INSERT INTO tb_decide VALUES('$uid','$uname','$cid')";
@@ -145,37 +175,10 @@ if ( !isset($_SESSION['urole']) || $_SESSION['urole']!=3 ) {
 
 
 		}
+
 		echo '<p><a href="cs_decide.php">戻る</a>';
 
-	}else
 
-	//一人ずつ////////////////////////////////////////////////////
-	if (isset($_POST['cid'])){
-		echo "hitoriboti";
-		if (isset($_POST['uid'])){
-			echo $cid=$_POST['cid'];
-			echo $uid=$_POST['uid'];
-			echo $uname = $_POST['uname'];
-			echo $act  = $_POST['act'] ;//送信されたactを受け取り、$actに代入
-
-			if ($act == 'insert'){//新規登録の場合
-				$sql = "INSERT INTO tb_decide VALUES('$uid','$uname','$cid')";
-				echo "<h2>登録しました</h2>";
-			}else{//再登録の場合
-				$sql = "UPDATE tb_decide SET uid='$uid', uname='$uname', cid='$cid' WHERE uid='$uid'";
-				echo "<h2>更新しました</h2>";
-			}
-			$rs = mysql_query($sql, $conn); //SQL文を実行
-			if (!$rs){
-				echo "<h2>登録が失敗しました</h2>";
-				echo mysql_error();
-			}else{
-				//header('cs_decide.php');
-			}
-		}else{ //エラーを表示
-			echo '<h2>エラー：希望コースまたは決定するユーザIDが選択されていません</h2>';
-			echo '<p><a href="cs_decide.php">戻る</a>';
-		}
 
 	}else{ //エラーを表示
 		echo '<h2>エラー：希望コースまたは決定するユーザIDが選択されていません</h2>';
