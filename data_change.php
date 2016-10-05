42d00561088ad6369997d21c64ad488863949274
annakawaguchi5 <k14jk037@st.kyusan-u.ac.jp>
2016-10-05 18:07:09 +0900

    10/5

---
+++ data_change.php
@@ -0,0 +1,63 @@
+<?php
+include('page_header.php');
+include_once('db_inc.php');
+
+if(isset($_GET['year'])){
+	$year=$_GET['year'];
+}
+
+$sql = "SELECT * FROM tb_limit NATURAL JOIN tb_course WHERE year='$year'";
+$rs = mysql_query($sql, $conn);
+if (!$rs) die ('エラー: ' . mysql_error());
+$row = mysql_fetch_array($rs) ;
+$stime=$row['stime'];
+$ltime=$row['ltime'];
+?>
+
+<div class="container" id="datachange" title="データ編集">
+	<form class="form-horizontal" action="upload.php"
+		method="post" onSubmit="return check(this)"
+		enctype="multipart/form-data" name="datachange">
+		<table id="form1" class="table table-bordered">
+			<tr>
+				<td class="header info">年度</td>
+				<td><?php echo $year;?></td>
+				<?php 	echo '<input type="hidden" name="year" value="'.$year.'">';?>
+			</tr>
+			<tr>
+				<td class="header info">調査開始時刻</td>
+				<td>
+					<div class='input-group date' id='stime'>
+						<span class="input-group-addon"> <span
+							class="glyphicon glyphicon-calendar"></span> </span>
+							<?php '<input
+							type="text" class="form-control" name="stime" value="'.$stime.'"/>';?>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="header info">調査終了時刻</td>
+				<td>
+					<div class='input-group date' id='ltime'>
+						<span class="input-group-addon"> <span
+							class="glyphicon glyphicon-calendar"></span> </span> <?php '<input
+							type="text" class="form-control" name="ltime" value="'.$ltime.'"/>';?>
+					</div>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="header info">コース</td>
+				<td><?php include('coursechange.php');?></td>
+			</tr>
+		</table>
+		<input type="hidden" name="act" value="insert">
+		<input type="submit" value="登録" />
+	</form>
+</div>
+
+
+
+</div>
+
+<?php include('page_footer.php');?>
--- new_year.php
+++ new_year.php
@@ -56,34 +56,6 @@ require_once('db_inc.php');  //データベース接続
 	</form>
 </div>
 
-
-<!-- #dialog-form-check -->
-<div id="dialog-form-check" title="フォーム確認" style="display: none;">
-	<p>
-		<b>Results:</b> <span id="results"></span>
-	</p>
-	<form>
-		<table id="form2" class="sample">
-			<tr>
-				<!-- <td>年度</td>
-				<td class="year"></td> -->
-				<td class="header year">年度</td>
-			</tr>
-			<tr>
-				<!-- <td>調査開始時刻</td>
-				<td class="stime"></td> -->
-				<td class="header stime">調査開始時刻</td>
-			</tr>
-			<tr>
-				<!--  <td>調査終了時刻</td>
-				<td class="ltime"></td> -->
-				<td class="header ltime">調査終了時刻</td>
-			</tr>
-		</table>
-	</form>
-</div>
-<!-- end of #dialog-form-check -->
-
 				<?php
 				include('page_footer.php');
 				?>
