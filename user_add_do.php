c63d1d4593543a2e239b79f4f7e266822a574ad2
annakawaguchi5 <k14jk037@st.kyusan-u.ac.jp>
2016-10-11 14:56:28 +0900

    10/11

--- user_add_do.php
+++ user_add_do.php
@@ -3,44 +3,44 @@ include('page_header.php');
 require_once ('db_inc.php');  // データベース接続
 
 if($_SESSION['urole']==9){
-$year=$_GET['year'];
-$uid = $_POST['uid']; //新規登録画面により送信されたユーザID
-$uname = $_POST['uname'];  //登録画面より送信されたユーザ名s
-$upass = $_POST['upass'];	//パスワード
-$urole = $_POST['urole'];	//種別
-if($_POST['halfgp']==0){
-	$halfgp = "NULL";
-}else{
-	$halfgp = $_POST['halfgp'];
-}
-if($_POST['halfgpa']==0){
-	$halfgpa = "NULL";
-}else{
-	$halfgpa = $_POST['halfgpa'];
-}
-if($_POST['allgp']==0){
-	$allgp = "NULL";
-}else{
-	$allgp = $_POST['allgp'];
-}
-if($_POST['allgpa']==0){
-	$allgpa = "NULL";
-}else{
-	$allgpa = $_POST['allgpa'];
-}
+	$year=$_GET['year'];
+	$uid = $_POST['uid']; //新規登録画面により送信されたユーザID
+	$uname = $_POST['uname'];  //登録画面より送信されたユーザ名s
+	$upass = $_POST['upass'];	//パスワード
+	$urole = $_POST['urole'];	//種別
+	if($_POST['halfgp']==""){
+		$halfgp = "NULL";
+	}else{
+		$halfgp = $_POST['halfgp'];
+	}
+	if($_POST['halfgpa']==0){
+		$halfgpa = "NULL";
+	}else{
+		$halfgpa = $_POST['halfgpa'];
+	}
+	if($_POST['allgp']==0){
+		$allgp = "NULL";
+	}else{
+		$allgp = $_POST['allgp'];
+	}
+	if($_POST['allgpa']==0){
+		$allgpa = "NULL";
+	}else{
+		$allgpa = $_POST['allgpa'];
+	}
 
 
-$where = 'WHERE 1';
-$sql = "INSERT INTO tb_user VALUES ($year, '$uid', '$uname', '$upass', $urole)";//検索条件を適用したSQL文を作成
-echo $sql;
-$rs = mysql_query($sql, $conn);
-if (!$rs)die ('エラー: ' . mysql_error());
-if($urole==1){
-	$sql = "INSERT INTO tb_gp VALUE ($year, '$uid', $halfgp, $halfgpa, $allgp, $allgpa)";
-	echo $sql;
+	$where = 'WHERE 1';
+	$sql = "INSERT INTO tb_user VALUES ($year, '$uid', '$uname', '$upass', $urole)";//検索条件を適用したSQL文を作成
+	//echo $sql;
 	$rs = mysql_query($sql, $conn);
 	if (!$rs)die ('エラー: ' . mysql_error());
-}
+	if($urole==1){
+		$sql = "INSERT INTO tb_gp VALUE ($year, '$uid', $halfgp, $halfgpa, $allgp, $allgpa)";
+		//echo $sql;
+		$rs = mysql_query($sql, $conn);
+		if (!$rs)die ('エラー: ' . mysql_error());
+	}
 }else{
 	echo '<h1 style="color:red">警告：あなたの権限ではこの機能を使えません。</h1>';
 }
--- user_change_do.php
+++ user_change_do.php
@@ -6,24 +6,42 @@ if (isset($_POST['change'])){
 	$uid = $_POST['change'];
 	$uname = $_POST['uname'];
 	$upass = $_POST['upass'];
-	$halfgp = $_POST['halfgp'];
-	$halfgpa = $_POST['halfgpa'];
-	$allgp = $_POST['allgp'];
-	$allgpa = $_POST['allgpa'];
+	if($_POST['halfgp']==0){
+		$halfgp = "NULL";
+	}else{
+		$halfgp = $_POST['halfgp'];
+	}
+	if($_POST['halfgpa']==0){
+		$halfgpa = "NULL";
+	}else{
+		$halfgpa = $_POST['halfgpa'];
+	}
+	if($_POST['allgp']==0){
+		$allgp = "NULL";
+	}else{
+		$allgp = $_POST['allgp'];
+	}
+	if($_POST['allgpa']==0){
+		$allgpa = "NULL";
+	}else{
+		$allgpa = $_POST['allgpa'];
+	}
+
 	foreach( $uid as $c=>$a  ) {
 			$output[] = array($uid[$c], $uname[$c],$upass[$c],$halfgp[$c],$halfgpa[$c],$allgp[$c],$allgpa[$c]);
 		}
 	foreach($output as $u=>$b){
-		/*
-		echo $uid=$b[0]."<br>";
-		echo $uname=$b[1]."<br>";
-		echo $upass=$b[2]."<br>";
-		echo $halfgp=$b[3]."<br>";
-		echo $halfgpa=$b[4]."<br>";
-		echo $allgp=$b[5]."<br>";
-		echo $allgpa=$b[6]."<br>";
-		*/
-		$sql = "UPDATE tb_user NATURAL JOIN tb_gp SET uname='$uname',upass='$upass',halfgp='$halfgp',halfgpa='$halfgpa',allgp='$allgp',allgpa='$allgpa' WHERE uid='$uid'";
+
+		$uid=$b[0];
+		$uname=$b[1];
+		$upass=$b[2];
+		echo $halfgp=$b[3];
+		echo $halfgpa=$b[4];
+		echo $allgp=$b[5];
+		echo $allgpa=$b[6];
+
+		$sql = "UPDATE tb_user NATURAL JOIN tb_gp SET uname='$uname',upass='$upass',halfgp=$halfgp,halfgpa=$halfgpa,allgp=$allgp,allgpa=$allgpa WHERE uid='$uid'";
+		echo $sql;
 		$rs = mysql_query($sql, $conn);
 		if (!$rs) die ('エラー: ' . mysql_error());
 
