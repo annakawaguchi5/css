85903d2a9b5fe8410be3e148cb31965c27cd8b2a
annakawaguchi5 <k14jk037@st.kyusan-u.ac.jp>
2016-10-27 18:58:27 +0900

    10/25

--- passwd_info.php
+++ passwd_info.php
@@ -7,26 +7,24 @@ include('page_header.php');
 	<p>パスワードを忘れてしまった場合 ここから記入してください。</p>
 	<p>すべて必須です</p>
 
-	■権限 <select id="urole" class="form-control">
-		<option>学生
-		<option>教員(コース決定権なし)
-		<option>教員(コース決定権あり)
-	</select>
-■ユーザID<INPUT type="text" name="gakuseki"> ■名前<INPUT type="text"
-				name="name">
+	■ユーザ種別
+	<div class="form-group">
+		<select id="urole" class="form-control">
+			<option value="1">学生
+
+			<option value="2">教員(コース決定権なし)
+
+			<option value="3">教員(コース決定権あり)
+
+		</select>
+	</div>
+	■ユーザID<INPUT type="text" name="gakuseki"> ■名前<INPUT type="text"
+		name="name"> <input type="submit" value="　送信　">
 
 
 
 
-
-
-
-<input type="submit" value="　送信　">
-
-
-
-
-		</form>
+</form>
 
 
 </Form>
--- passwd_info_do.php
+++ passwd_info_do.php
@@ -1,22 +1,23 @@
 <?php include('page_header.php');
 include_once('db_inc.php');
 
+$urole=$_POST['$urole'];
 $uid=$_POST['gakuseki'];
 $uname=$_POST['name'];
 
 
 
-$sql ="select uid from passwd_info where uid='$uid'";
-$rs = mysql_query( $sql, $conn );
-if (!$rs) die ('エラー: ' . mysql_error());
-$row = mysql_fetch_array($rs) ;
-
-if($uid=="" || $uname==""){
-	echo "ユーザID、名前を記入してください。".'<br>';
+/*
+ $sql ="select uid from passwd_info where uid='$uid'";
+ $rs = mysql_query( $sql, $conn );
+ if (!$rs) die ('エラー: ' . mysql_error());
+ $row = mysql_fetch_array($rs) ;
+ */
+if($urole=="" || $uid=="" || $uname==""){
+	echo "ユーザ種別、ユーザID、名前を記入してください。".'<br>';
 	echo '<h4><a href="passwd_info.php">戻る</a></h4>';
 }else{
-
-	$sql ="insert into passwd_info(uid, uname, timestamp, reset) values ('$uid','$uname',now(), 0)";
+	$sql ="insert into passwd_info(uid, uname, urole, timestamp, reset) values ('$uid','$uname',$urole,now(), 0)";
 	$res = mysql_query( $sql, $conn );
 	if (!$res) {
 		echo "送信に失敗しました。";
